f40bc6dfb2c1c40963d3457b7687bf86cf33a7d5
korafdavid <okoroafordavid61@gmail.com>
2021-11-17 22:21:44 +0100

    latest update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/services.dart';
 import 'package:desktop_dialog/desktop_dialog.dart';
 
 void main() {
-  runApp(const MaterialApp(home:  MyApp()));
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -16,35 +16,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await DesktopDialog.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -55,43 +27,56 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Center(
-              child: Text('Running on: $_platformVersion\n'),
-            ),
-            MaterialButton(onPressed: (){
-              createDialog(context,
-              title: 'Warning!',
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Icon(Icons.clear, color: Colors.red, size: 40),
-                  ),
-                   Flexible(
-                     child: Text('Command Pubspec Assits: Add/Update Dependencies resulted in an error. (Running the Contributed Command \'pubspec-assit.addDependency\' failed )',
-                     softWrap: true ,
-                     ),
-                   ),
-                ],
-              )
-              );
-            },
-            onLongPress: (){
-              showDialog(context: context, builder: (context){
-                return AlertDialog(
-                  title: const Text('BDUBDIBDIBDISDBIDBI'),
-                  content: IntrinsicHeight(
-                    child: Column(
-                      children:const [
-                        Text('ddfimffdfimdkfdfifmdkm')
-                      ]
-                    ),
-                  ),
-                );
-              });
-            },
-            child: const Text('show Dialog'),
+           
+            MaterialButton(
+              onPressed: () {
+                createDialog(context,
+                    title: 'Warning!',
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(Icons.clear, color: Colors.red, size: 40),
+                        ),
+                        Flexible(
+                          child: Text(
+                            'Command Pubspec Assits: Add/Update Dependencies resulted in an error. (Running the Contributed Command \'pubspec-assit.addDependency\' failed )',
+                            softWrap: true,
+                          ),
+                        ),
+                      ],
+                    ));
+              },
+              onLongPress: () {
+                createDialog(context,
+                    child:  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Ink(
+                        decoration:  BoxDecoration(border: Border.all(color: Colors.black)),
+                        child: const LinearProgressIndicator(
+                            minHeight: 20, 
+                            backgroundColor: Colors.transparent,
+                            value: 0.95,
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                            ),
+                      ),
+                    ));
+                //SimpleDesktopDialog(context: context, child: const Text('dfwrsf'));
+                // showDialog(context: context, builder: (context){
+                //   return AlertDialog(
+                //     title: const Text('BDUBDIBDIBDISDBIDBI'),
+                //     content: IntrinsicHeight(
+                //       child: Column(
+                //         children:const [
+                //           Text('ddfimffdfimdkfdfifmdkm')
+                //         ]
+                //       ),
+                //     ),
+                //   );
+                // });
+              },
+              child: const Text('show Dialog'),
             )
           ],
         ),
@@ -99,4 +84,3 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
-
